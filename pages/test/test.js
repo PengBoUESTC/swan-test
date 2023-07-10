592d6859adc83c4a933e8311fd7c56249904c4b9
pengbo43 <pengbo43@jd.com>
2023-07-10 14:41:12 +0800

    chore: add test

--- pages/test/test.js
+++ pages/test/test.js
@@ -9,9 +9,16 @@ Page({
     },
     handleGo() {
         swan.navigateBack({
+          // delta: -2,
           success: () => {
             console.log('=successsuccesssuccess')
-          }
+          },
+          complete: () => {
+            console.log('=completecompletecompletecomplete')
+          },
+          fail: () => {
+            console.log('=failfailfailfail')
+          },
         })
     }
 })
